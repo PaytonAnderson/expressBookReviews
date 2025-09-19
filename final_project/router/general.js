b01ff930b0063e4342159e798b8c8e9ba546c214
PaytonAnderson <payton@clickityclick.com>
2025-09-19 14:55:59 -0400

    tasks 1-4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,38 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let ret = JSON.stringify(books, null, 4)
+    res.send(ret)
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author
+  let ret = []
+  for (let isbn in books) {
+    if (books[isbn].author === author){
+        ret.push({isbn, ...books[isbn]})
+    }
+  }
+  res.send(ret)
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title
+    let ret = []
+    for (let isbn in books) {
+      if (books[isbn].title === title){
+          ret.push({isbn, ...books[isbn]})
+      }
+    }
+    res.send(ret)
 });
 
 //  Get book review
